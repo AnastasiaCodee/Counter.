4acc45a7ad078507fc1733c35d87524d1db3b673
AnastasiaCodee <nastena.kirichenko.06@mail.ru>
2026-01-14 14:20:44 +0400

    Добавлен проект Counter

--- Counter./ViewController.swift
+++ Counter./ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    private var score = 0
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        score += 1
+        scoreLabel.text = "Значение счётчика: \(score)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        score = 0
+        scoreLabel.text = "0"
+        
     }
-
-
 }
-
+    
